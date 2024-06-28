06d9ad600a404366e63990666361181c7b741ef3
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2024-06-28 15:09:54 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -116,8 +116,10 @@ def on_message(message, client):
 		except Exception as e:
 			message.message.reply("Something went wrong while running the crypto computer:\n\n" + str(e))
 
-	if "🐟" in message.content and "The Deep Wizard" in message.content and "quivers" in message.content and int(message.user.id) == 375672:
+	if "🐟" in message.content and "Petəíŕd The Wizard" in message.content and "quivers" in message.content and int(message.user.id) == 375672:
 		message.room.send_message("/fish again")
+	if "📧" in message.content and "Petəíŕd The Wizard" in message.content and "inbox pings" in message.content and int(message.user.id) == 375672:
+		message.room.send_message("/phish again")
 
 def setup_logging():
 	logging.basicConfig(level=logging.CRITICAL)
